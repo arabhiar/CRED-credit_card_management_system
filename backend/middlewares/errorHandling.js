125dca3fbe1b29e1962428c5b081e718f38d907e
Narendra manglani <narendramanglani04@gmail.com>
2021-03-22 14:41:43 +0530

    Error Handling string

--- backend/middlewares/errorHandling.js
+++ backend/middlewares/errorHandling.js
@@ -1,15 +1,3 @@
-// function errorHandler(err, req, res, next) {
-//     
-//     switch (true) {
-//         default:
-//             statusCode = res.statusCode;
-//             return res.status(statusCode).json({ message: err.message });
-//     }
-// }
-
-
-// module.exports = errorHandler;
-
 function errorHandler(err, req, res, next) {
     let statusCode = 500;
     switch (true) {
