0ba6e021d99f64b0c3f5aa18c157519e5330ac40
Narendra manglani <narendramanglani04@gmail.com>
2021-04-07 02:25:11 +0530

    reminders updated

--- backend/services/card.js
+++ backend/services/card.js
@@ -339,29 +339,27 @@ module.exports = {
                         },
                     },
                     attributes: ['transactionId', 'amount', 'vendor', 'credDeb', 'category', 'transactionDateTime', 'userAssociated']
-                })  
-                    .then((data) => {
-                        data.sort(function(a, b) {
-                            if(a.transactionDateTime > b.transactionDateTime)
-                                return 1;
-                            if(a.transactionDateTime < b.transactionDateTime)
-                                return -1;
-                            return 0;
-                        });
-                        // Pagination
-                        const perPage = 2;
-                        const page = Number(req.query.pageNumber) || 1;
-                        const count = data.length;
-                        const pages = Math.ceil(count / perPage);
-                        const indexOfLastStatement = page * perPage;
-                        const indexOfFirstStatement = indexOfLastStatement - perPage;
-                        const currentStatements = data.slice(indexOfFirstStatement, indexOfLastStatement);
-                        res.status(200).json({data: currentStatements, pages, page});
-                    })
-                    .catch((err) => {
-                        res.statusCode = 500;
-                        throw new Error(err);
-                    })
+                }).catch((err) => {
+                    res.statusCode = 500;
+                    throw new Error(err);
+                })
+            
+                statements.sort(function(a, b) {
+                    if(a.transactionDateTime > b.transactionDateTime)
+                        return 1;
+                    if(a.transactionDateTime < b.transactionDateTime)
+                        return -1;
+                    return 0;
+                });
+                // Pagination
+                const perPage = 2;
+                const page = Number(req.query.pageNumber) || 1;
+                const count = statements.length;
+                const pages = Math.ceil(count / perPage);
+                const indexOfLastStatement = page * perPage;
+                const indexOfFirstStatement = indexOfLastStatement - perPage;
+                const currentStatements = statements.slice(indexOfFirstStatement, indexOfLastStatement);
+                res.status(200).json({data: currentStatements, pages, page});
                 return;
             }
         }
@@ -410,20 +408,18 @@ module.exports = {
                     },
                     attributes: ['transactionId', 'amount', 'vendor', 'credDeb', 'category', 'transactionDateTime', 'userAssociated']
                 })  
-                    .then((data) => {
-                        data.sort(function(a, b) {
-                            if(a.transactionDateTime > b.transactionDateTime)
-                                return 1;
-                            if(a.transactionDateTime < b.transactionDateTime)
-                                return -1;
-                            return 0;
-                        });
-                        res.status(200).send(data);
-                    })
-                    .catch((err) => {
-                        res.statusCode = 500;
-                        throw new Error(err);
-                    })
+                .catch((err) => {
+                    res.statusCode = 500;
+                    throw new Error(err);
+                })
+                statements.sort(function(a, b) {
+                    if(a.transactionDateTime > b.transactionDateTime)
+                        return 1;
+                    if(a.transactionDateTime < b.transactionDateTime)
+                        return -1;
+                    return 0;
+                });
+                res.status(200).send(statements);
                 return;
             }
         }
@@ -517,73 +513,70 @@ module.exports = {
 
             // if we get the same card number associated with the currentLoggedIn user.
             if(currentCardNumber === req.params.id) {
-                await db.Transaction.findAll({
+                const allStatements = await db.Transaction.findAll({
                     where: {
                         CardId: profileCardId.CardId,
                     },
                     attributes: ['transactionId', 'amount', 'vendor', 'credDeb', 'category', 'transactionDateTime', 'userAssociated']
                 })
-                    .then((allStatements) => {
-                        const allCategories = new Set();
-                        const allVendors = new Set();
+                .catch((err) => {
+                    res.statusCode = 500;
+                    throw new Error(err);
+                })
+                    
+                const allCategories = new Set();
+                const allVendors = new Set();
 
-                        for(const statement of allStatements) {
-                            allCategories.add(statement.category);
-                            allVendors.add(statement.vendor);
+                for(const statement of allStatements) {
+                    allCategories.add(statement.category);
+                    allVendors.add(statement.vendor);
+                }
+
+
+                let labels = [];
+                let data = [];
+                for(let currCategory of allCategories) {
+                    labels.push(currCategory);
+                    let totalAmount = 0;
+                    for(let statement of allStatements) {
+                        if(statement.category === currCategory) {
+                            totalAmount += parseFloat(statement.amount);
                         }
+                    }
+                    data.push(totalAmount);
+                }
 
+                
+                const categories = {
+                    labels: [...labels],
+                    data: [...data]
+                };
 
-                        let labels = [];
-                        let data = [];
-                        for(let currCategory of allCategories) {
-                            labels.push(currCategory);
-                            let totalAmount = 0;
-                            for(let statement of allStatements) {
-                                if(statement.category === currCategory) {
-                                    totalAmount += parseFloat(statement.amount);
-                                }
-                            }
-                            data.push(totalAmount);
+                labels = [];
+                data = [];
+
+                for(let currVendor of allVendors) {
+                    labels.push(currVendor);
+                    let totalAmount = 0;
+                    for(let statement of allStatements) {
+                        if(statement.vendor === currVendor) {
+                            totalAmount += parseFloat(statement.amount);
                         }
+                    }
+                    data.push(totalAmount);
+                }
 
-                        
-                        const categories = {
-                            labels: [...labels],
-                            data: [...data]
-                        };
+                const vendors = {
+                    labels: [...labels],
+                    data: [...data]
+                }
 
-                        labels = [];
-                        data = [];
+                const smartStatement = {
+                    categories: categories,
+                    vendors: vendors
+                }
 
-                        for(let currVendor of allVendors) {
-                            labels.push(currVendor);
-                            let totalAmount = 0;
-                            for(let statement of allStatements) {
-                                if(statement.vendor === currVendor) {
-                                    totalAmount += parseFloat(statement.amount);
-                                }
-                            }
-                            data.push(totalAmount);
-                        }
-
-                        const vendors = {
-                            labels: [...labels],
-                            data: [...data]
-                        }
-
-                        const smartStatement = {
-                            categories: categories,
-                            vendors: vendors
-                        }
-
-                        res.send(smartStatement);
-
-
-                    })
-                    .catch((err) => {
-                        res.statusCode = 500;
-                        throw new Error(err);
-                    })
+                res.send(smartStatement);
                 return;
             }
         }
@@ -635,7 +628,7 @@ module.exports = {
 
             // if we get the same card number associated with the currentLoggedIn user.
             if(currentCardNumber === req.params.id) {
-                const statements = await db.Transaction.findAll({
+                const allStatements = await db.Transaction.findAll({
                     where: {
                         CardId: profileCardId.CardId,
                         transactionDateTime: { // we are now fetching all the statements between the starting and endingDate
@@ -645,83 +638,81 @@ module.exports = {
                     },
                     attributes: ['transactionId', 'amount', 'vendor', 'credDeb', 'category', 'transactionDateTime', 'userAssociated']
                 })  
-                    .then((allStatements) => {
-                        const allCategories = new Set();
-                        const allVendors = new Set();
-
-                        for(const statement of allStatements) {
-                            allCategories.add(statement.category);
-                            allVendors.add(statement.vendor);
-                        }
-
-
-                        let labels = [];
-                        let data = [];
-                        let count = [];
-                        for(let currCategory of allCategories) {
-                            labels.push(currCategory);
-                            let totalAmount = 0;
-                            let currentCount = 0;
-                            for(let statement of allStatements) {
-                                if(statement.category === currCategory) {
-                                    totalAmount += parseFloat(statement.amount);
-                                    currentCount += 1;
-                                }
-                            }
-                            data.push(totalAmount);
-                            count.push(currentCount);
-                        }
-                        
-                        const categories = {
-                            labels: [...labels],
-                            data: [...data]
-                        };
-
-                        const categoriesCount = {
-                            labels: [...labels],
-                            data: [...count]
-                        }
-
-                        labels = [];
-                        data = [];
-                        count = [];
-
-                        for(let currVendor of allVendors) {
-                            labels.push(currVendor);
-                            let totalAmount = 0;
-                            let currentCount = 0;
-                            for(let statement of allStatements) {
-                                if(statement.vendor === currVendor) {
-                                    totalAmount += parseFloat(statement.amount);
-                                    currentCount += 1;
-                                }
-                            }
-                            data.push(totalAmount);
-                            count.push(currentCount);
-                        }
-
-                        const vendors = {
-                            labels: [...labels],
-                            data: [...data]
-                        }
-
-                        const vendorsCount = {
-                            labels: [...labels],
-                            data: [...count]
-                        }
-
-                        const smartStatement = {
-                            categories: categories,
-                            vendors: vendors,
-                            categoriesCount: categoriesCount,
-                            vendorsCount: vendorsCount
-                        }
-                        res.send(smartStatement);
-                    })
                     .catch((err) => {
                         res.statusCode = 500;
                         throw new Error(err);
                     })
+                const allCategories = new Set();
+                const allVendors = new Set();
+
+                for(const statement of allStatements) {
+                    allCategories.add(statement.category);
+                    allVendors.add(statement.vendor);
+                }
+
+
+                let labels = [];
+                let data = [];
+                let count = [];
+                for(let currCategory of allCategories) {
+                    labels.push(currCategory);
+                    let totalAmount = 0;
+                    let currentCount = 0;
+                    for(let statement of allStatements) {
+                        if(statement.category === currCategory) {
+                            totalAmount += parseFloat(statement.amount);
+                            currentCount += 1;
+                        }
+                    }
+                    data.push(totalAmount);
+                    count.push(currentCount);
+                }
+                
+                const categories = {
+                    labels: [...labels],
+                    data: [...data]
+                };
+
+                const categoriesCount = {
+                    labels: [...labels],
+                    data: [...count]
+                }
+
+                labels = [];
+                data = [];
+                count = [];
+
+                for(let currVendor of allVendors) {
+                    labels.push(currVendor);
+                    let totalAmount = 0;
+                    let currentCount = 0;
+                    for(let statement of allStatements) {
+                        if(statement.vendor === currVendor) {
+                            totalAmount += parseFloat(statement.amount);
+                            currentCount += 1;
+                        }
+                    }
+                    data.push(totalAmount);
+                    count.push(currentCount);
+                }
+
+                const vendors = {
+                    labels: [...labels],
+                    data: [...data]
+                }
+
+                const vendorsCount = {
+                    labels: [...labels],
+                    data: [...count]
+                }
+
+                const smartStatement = {
+                    categories: categories,
+                    vendors: vendors,
+                    categoriesCount: categoriesCount,
+                    vendorsCount: vendorsCount
+                }
+                res.send(smartStatement);
                 return;
             }
         }
